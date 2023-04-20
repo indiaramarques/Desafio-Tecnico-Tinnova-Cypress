1c4940eb81b277423885024004517e41eded96e6
Indiara Caetano Marques <76412627+indiaramarques@users.noreply.github.com>
2023-04-19 21:48:00 -0300

    🔧 Ajusta suite de teste

--- cypress/e2e/Suite2.cy.js
+++ cypress/e2e/Suite2.cy.js
@@ -10,7 +10,8 @@ describe('Suite 2 - Desafio nível 2', () => {
 
     it('Cenário 1 - Edição de usuário', () => {
         cy.contains('Entrar').click()
-        cy.get(':nth-child(1) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(3)').click()
+        cy.wait(3000)
+        cy.get('#root > div > div.sc-jqUVSM.jkrDKT > div.sc-jSMfEi.iuhvyf > div:nth-child(1) > div:nth-child(5) > div:nth-child(3)').click()
         cy.get('input[name=name]').clear().type('Juliana Ferreira de Melo')
         cy.get('input[name=email]').clear().type('teste@email.com')
         cy.get('input[name=cpf]').clear().type('76227255017')
@@ -20,7 +21,9 @@ describe('Suite 2 - Desafio nível 2', () => {
 
     it('Cenário 2 - Deletar usuário', () => {
         cy.contains('Entrar').click()
-        cy.get(':nth-child(3) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(4)').click()
+        cy.wait(3000)
+        cy.get('#root > div > div.sc-jqUVSM.jkrDKT > div.sc-jSMfEi.iuhvyf > div:nth-child(1) > div:nth-child(5) > div:nth-child(4)').click()
+        cy.contains('Deseja mesmo remover?').should('exist')
         cy.get(':nth-child(2) > .swal-button').click()
     })
 })
